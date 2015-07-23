bc6504d65b218c82c29e55a2c547a63c674e793e
Ben Hanzl <benh@izea.com>
2015-07-23 14:17:33 -0400

    Layout initial interface for SignInViewController.

---
+++ OnePasswordExample/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  OnePasswordExample
+//
+//  Created by Ben Hanzl on 7/23/15.
+//  Copyright © 2015 Ben Hanzl. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
