33b95fbe3c1b4eaa852da01062a2a15e7e5145a5
Ben Hanzl <benh@izea.com>
2015-07-23 14:03:00 -0400

    Create a new project.

---
+++ OnePasswordExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnePasswordExample
+//
+//  Created by Ben Hanzl on 7/23/15.
+//  Copyright © 2015 Ben Hanzl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
